38c2838618f19e8307303a2578357f1b68d99a73
Anuj Jaha <er.anujjaha@gmail.com>
2018-04-27 10:29:17 +0530

    Removed duplicate fields

--- resources/views/common/career/form.blade.php
+++ resources/views/common/career/form.blade.php
@@ -42,14 +42,7 @@
         </div>
     </div>
 </div>
-<div class="box-body">
-    <div class="form-group">
-        {{ Form::label('description', 'Description :', ['class' => 'col-lg-2 control-label']) }}
-        <div class="col-lg-10">
-            {{ Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Description', 'required' => 'required']) }}
-        </div>
-    </div>
-</div>
+
 <div class="box-body">
     <div class="form-group">
         {{ Form::label('icon', 'Icon :', ['class' => 'col-lg-2 control-label']) }}
